ef584f349f4acd1d1c29da2c7eecf94098e2a49b
milkyfuckinway <0-0_0@bk.ru>
2022-12-18 22:41:04 +0300

    fix

--- src/js/modules/note.js
+++ src/js/modules/note.js
@@ -1,8 +1,8 @@
-const note = document.querySelector('.note');
-const noteHeader = document.querySelector('.note__header');
+const noteList = document.querySelectorAll('.note');
 
 let initialX = 0;
 let initialY = 0;
+let initialzIndex = 0;
 let moveElement = false;
 
 const events = {
@@ -12,11 +12,10 @@ const events = {
     up: 'mouseup',
   },
   touch: {
-
     down: 'touchstart',
     move: 'touchmove',
     up: 'touchend',
-  }
+  },
 };
 
 let deviceType = '';
@@ -26,8 +25,7 @@ const isTouchDevice = () => {
     document.createEvent('TouchEvent');
     deviceType = 'touch';
     return true;
-  }
-  catch(err) {
+  } catch (err) {
     deviceType = 'mouse';
     return false;
   }
@@ -35,36 +33,54 @@ const isTouchDevice = () => {
 
 isTouchDevice();
 
-noteHeader.addEventListener(events[deviceType].down, (evt) => {
-  if (evt.target === noteHeader) {
-    evt.preventDefault();
-    initialX = !isTouchDevice() ? evt.clientX : evt.touches[0].clientX;
-    initialY = !isTouchDevice() ? evt.clientY : evt.touches[0].clientY;
-    moveElement = true;
-    document.addEventListener(events[deviceType].move, onMoveEvent);
+noteList.forEach((item) => {
+  item.addEventListener(events[deviceType].down, (evt) => {
+    if (evt.target.classList.contains('note__header')) {
+      evt.preventDefault();
+      initialX = !isTouchDevice() ? evt.clientX : evt.touches[0].clientX;
+      initialY = !isTouchDevice() ? evt.clientY : evt.touches[0].clientY;
+      moveElement = true;
+      document.addEventListener(events[deviceType].move, onMoveEvent);
+      const newzIndex = initialzIndex + 2;
+      item.style.zIndex = `${newzIndex}`;
+      initialzIndex = newzIndex;
+    }});
+
+  const stopMovement = () => {
+    moveElement = false;
+    console.log('stopmovement');
+    document.removeEventListener(events[deviceType].move, onMoveEvent);
+  };
+
+  function onMoveEvent(evt) {
+    console.log('onMoveEvent');
+    if (moveElement) {
+      evt.preventDefault();
+      const newX = !isTouchDevice() ? evt.clientX : evt.touches[0].clientX;
+      const newY = !isTouchDevice() ? evt.clientY : evt.touches[0].clientY;
+      item.style.left = `${item.offsetLeft - (initialX - newX)}px`;
+      item.style.top = `${item.offsetTop - (initialY - newY)}px`;
+      initialX = newX;
+      initialY = newY;
+      document.addEventListener('mouseleave', stopMovement);
+      item.addEventListener(events[deviceType].up, stopMovement);
+    }
   }
 });
-
-const stopMovement = () => {
-  moveElement = false;
-  console.log('stopmovement');
-  document.removeEventListener(events[deviceType].move, onMoveEvent);
-}
-
-function onMoveEvent (evt) {
-  console.log('onMoveEvent');
-  if(moveElement) {
-    evt.preventDefault();
-    const newX = !isTouchDevice() ? evt.clientX : evt.touches[0].clientX;
-    const newY = !isTouchDevice() ? evt.clientY : evt.touches[0].clientY;
-    note.style.top = `${note.offsetTop - (initialY - newY) }px`;
-    note.style.left = `${note.offsetLeft - (initialX - newX) }px`;
-    initialX = newX;
-    initialY = newY;
-    document.addEventListener('mouseleave', stopMovement);
-    note.addEventListener(events[deviceType].up, stopMovement);
+let firstPositionX = 50;
+let firstPositionY = 50;
+const randomPosition = () => {
+  noteList.forEach((item) => {
+    const newPositionX = firstPositionX + 25;
+    const newPositionY = firstPositionY + 35;
+    const newzIndex = initialzIndex + 2;
+    item.style.left = `${newPositionX}px`;
+    item.style.top = `${newPositionY}px`;
+    item.style.zIndex = newzIndex;
+    firstPositionX = newPositionX;
+    firstPositionY = newPositionY;
+    initialzIndex = newzIndex;
   }
+  );
 };
-
-
-
+randomPosition();
