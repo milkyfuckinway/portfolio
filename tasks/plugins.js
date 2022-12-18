559d75b7611b7ef33f33b04c64ba5ed4b34dd110
milkyfuckinway <0-0_0@bk.ru>
2022-12-18 13:45:21 +0300

    start

---
+++ tasks/plugins.js
@@ -0,0 +1,8 @@
+import gulp from 'gulp';
+import path from '../config/path.js';
+
+export default () => {
+  console.log('Перенос плагинов');
+  return gulp.src(path.plugins.src)
+    .pipe(gulp.dest(path.plugins.dest));
+};
