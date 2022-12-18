4f6e19e66060dc5ff312a23e9739ce497aa41e67
milkyfuckinway <0-0_0@bk.ru>
2022-12-18 13:46:11 +0300

    fix

--- build/js/main.js
+++
@@ -1,3 +0,0 @@
-
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiJdLCJmaWxlIjoibWFpbi5qcyJ9
