2e41beae3ba3c1983b6deedc90e224920276c615
milkyfuckinway <0-0_0@bk.ru>
2022-12-26 23:21:35 +0300

    fix

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,1 @@
-import './modules/loading.js';
-import './modules/window.js';
-
-//# sourceMappingURL=main.js.map
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnLi9tb2R1bGVzL2xvYWRpbmcuanMnO1xyXG5pbXBvcnQgJy4vbW9kdWxlcy93aW5kb3cuanMnO1xyXG4iXSwiZmlsZSI6Im1haW4uanMifQ==
+import"./modules/loading.js";import"./modules/window.js";
