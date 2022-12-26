03ddf26fc4e8a87bc0626adec5114b9ec5bc74e3
milkyfuckinway <0-0_0@bk.ru>
2022-12-26 17:58:41 +0300

    fix

--- src/js/modules/window.js
+++ src/js/modules/window.js
@@ -7,7 +7,6 @@ const template = document.querySelector('.template');
 const desktop = document.querySelector('.desktop');
 const desktopFooter = desktop.querySelector('.desktop__footer');
 const windowTemplate = template.querySelector('.window');
-const fileList = document.querySelectorAll('.file');
 const referenceTemplate = template.querySelector('.reference');
 let initialzIndex = 0;
 let initialX = 0;
@@ -22,6 +21,15 @@ let offsetHorisontalCounter = 0;
 desktopFooter.addEventListener('wheel', (evt) => {
   evt.preventDefault();
   desktopFooter.scrollLeft += evt.deltaY;
+});
+
+desktop.addEventListener(events[deviceType].click, (evt) => {
+  if (evt.target.closest('.file')) {
+    onFileOpen(evt);
+  }
+  if (evt.target.closest('.reference')) {
+    console.log('reference');
+  }
 });
 
 const setStartPosition = (elem) => {
@@ -46,154 +54,152 @@ const setStartPosition = (elem) => {
   }
 };
 
-fileList.forEach((item) => {
-  if (item.classList.contains('file--text') || item.classList.contains('file--folder')) {
-    item.addEventListener(events[deviceType].click, onFileOpen);
+function onFileOpen(evt) {
+  evt.target.classList.remove('file');
+  evt.target.classList.add('file--opened');
+  const fileLabel = evt.target.querySelector('.file__label');
+  const fileName = evt.target.querySelector('.file__name');
+  const pathIcon = evt.target.querySelector('.file__icon').cloneNode(true);
+  const newWindow = windowTemplate.cloneNode(true);
+  const newWindowPath = newWindow.querySelector('.window__path');
+  const windowHeader = newWindow.querySelector('.window__header');
+  const windowDraggableArea = windowHeader.querySelector('.window__draggable-area');
+  const reference = referenceTemplate.cloneNode(true);
+
+  const clonedTargetContent = evt.target.querySelector('.file__content').cloneNode(true);
+  newWindow.appendChild(clonedTargetContent);
+  clonedTargetContent.classList.remove('visually-hidden');
+  clonedTargetContent.classList.add('window__content');
+  clonedTargetContent.classList.remove('file__content');
+  if (evt.target.classList.contains('file--folder')) {
+    clonedTargetContent.classList.add('window__content--folder');
   }
-  function onFileOpen(evt) {
-    evt.target.removeEventListener(events[deviceType].click, onFileOpen);
-    const fileLabel = evt.target.querySelector('.file__label');
-    const fileName = evt.target.querySelector('.file__name');
-    const pathIcon = evt.target.querySelector('.file__icon').cloneNode(true);
-    const newWindow = windowTemplate.cloneNode(true);
-    const newWindowPath = newWindow.querySelector('.window__path');
-    const windowHeader = newWindow.querySelector('.window__header');
-    const windowDraggableArea = windowHeader.querySelector('.window__draggable-area');
-    const reference = referenceTemplate.cloneNode(true);
-    const clonedTargetContent = evt.target.querySelector('.file__content').cloneNode(true);
-    const referenceIcon = evt.target.querySelector('.file__icon').cloneNode(true);
-    newWindowPath.textContent = fileName.textContent;
-    windowDraggableArea.insertBefore(pathIcon, newWindowPath);
-    desktop.appendChild(newWindow);
-    setStartPosition(newWindow);
-    fileLabel.classList.add('file__label--active');
 
-    const setCurrentWindowActive = () => {
-      lastFile = reference;
-      const newzIndex = initialzIndex + 1;
-      newWindow.style.zIndex = `${newzIndex}`;
-      initialzIndex = newzIndex;
-      desktop.querySelectorAll('.window--active').forEach((a) => {
-        a.classList.remove('window--active');
-      });
-      newWindow.classList.add('window--active');
-      desktop.querySelectorAll('.reference').forEach((b) => {
-        if (b === lastFile) {
-          b.classList.add('reference--active');
-        } else {
-          b.classList.remove('reference--active');
-        }
-      });
-    };
+  const referenceIcon = evt.target.querySelector('.file__icon').cloneNode(true);
+  newWindowPath.textContent = fileName.textContent;
+  windowDraggableArea.insertBefore(pathIcon, newWindowPath);
+  desktop.appendChild(newWindow);
+  setStartPosition(newWindow);
+  fileLabel.classList.add('file__label--active');
 
-    setCurrentWindowActive();
-
-    const onMoveEvent = (e) => {
-      if (moveElement === true) {
-        const newX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
-        const newY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
-        newWindow.style.left = `${newWindow.offsetLeft - (initialX - newX)}px`;
-        newWindow.style.top = `${newWindow.offsetTop - (initialY - newY)}px`;
-        initialX = newX;
-        initialY = newY;
-      }
-    };
-
-    const onMoveStop = () => {
-      document.removeEventListener(events[deviceType].move, onMoveEvent);
-      document.removeEventListener(events[deviceType].up, onMoveStop);
-      moveElement = false;
-    };
-
-    function onWindowDrag(e) {
-      setCurrentWindowActive(reference, newWindow);
-      if (e.cancelable) {
-        e.preventDefault();
-      }
-      if (!newWindow.classList.contains('window--fullscreen')) {
-        moveElement = true;
-        initialX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
-        initialY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
-        document.addEventListener(events[deviceType].move, onMoveEvent);
-        document.addEventListener(events[deviceType].up, onMoveStop);
-      }
-    }
-    const onCollapseButton = () => {
-      setCurrentWindowActive();
-      if (newWindow.classList.contains('window--collapsed')) {
-        newWindow.classList.remove('window--collapsed');
-        setCurrentWindowActive();
+  const setCurrentWindowActive = () => {
+    lastFile = reference;
+    const newzIndex = initialzIndex + 1;
+    newWindow.style.zIndex = `${newzIndex}`;
+    initialzIndex = newzIndex;
+    desktop.querySelectorAll('.window--active').forEach((a) => {
+      a.classList.remove('window--active');
+    });
+    newWindow.classList.add('window--active');
+    desktop.querySelectorAll('.reference').forEach((b) => {
+      if (b === lastFile) {
+        b.classList.add('reference--active');
       } else {
-        if (reference === lastFile) {
-          newWindow.classList.add('window--collapsed');
-        } else {
-          setCurrentWindowActive();
-        }
-      }
-    };
-
-    const onCloseButton = () => {
-      setCurrentWindowActive();
-      newWindow.remove();
-      pathIcon.remove();
-      item.addEventListener(events[deviceType].click, onFileOpen);
-      reference.remove();
-      clonedTargetContent.remove();
-      referenceIcon.remove();
-      fileLabel.classList.remove('file__label--active');
-      if (offsetVerticalCounter > 0) {
-        offsetVerticalCounter -= 1;
-        initialWindowCounterVertical -= 30;
-      }
-      if (offsetHorisontalCounter > 0) {
-        offsetHorisontalCounter -= 1;
-        initialWindowCounterHorisontal -= 10;
-      }
-    };
-
-    const onExpandButton = () => {
-      setCurrentWindowActive(reference, newWindow);
-      if (newWindow.classList.contains('window--fullscreen')) {
-        newWindow.classList.remove('window--fullscreen');
-      } else {
-        newWindow.classList.add('window--fullscreen');
-      }
-    };
-
-    newWindow.addEventListener(events[deviceType].down, (e) => {
-      if (e.target.closest('.window__draggable-area')) {
-        onWindowDrag(e);
-      }
-      if (e.target.closest('.window__button--collapse')) {
-        onCollapseButton();
-      }
-      if (e.target.closest('.window__button--close')) {
-        onCloseButton();
-      }
-      if (e.target.closest('.window__button--expand')) {
-        onExpandButton();
-      }
-      if (e.target.closest('.window__content')) {
-        setCurrentWindowActive();
+        b.classList.remove('reference--active');
       }
     });
+  };
 
-    newWindow.appendChild(clonedTargetContent);
-    clonedTargetContent.classList.remove('visually-hidden');
-    clonedTargetContent.classList.add('window__content');
-    clonedTargetContent.classList.remove('file__content');
-    if (evt.target.classList.contains('file--folder')) {
-      const innerFiles = newWindow.querySelectorAll('.file');
-      clonedTargetContent.classList.add('window__content--folder');
-      innerFiles.forEach((file) => {
-        file.addEventListener(events[deviceType].click, onFileOpen);
-      });
+  setCurrentWindowActive();
+
+  const onMoveEvent = (e) => {
+    if (moveElement === true) {
+      const newX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+      const newY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+      newWindow.style.left = `${newWindow.offsetLeft - (initialX - newX)}px`;
+      newWindow.style.top = `${newWindow.offsetTop - (initialY - newY)}px`;
+      initialX = newX;
+      initialY = newY;
     }
+  };
 
-    const referenceText = reference.querySelector('.reference__text');
-    referenceText.textContent = fileName.textContent;
-    reference.insertBefore(referenceIcon, referenceText);
-    desktopFooter.appendChild(reference);
-    reference.addEventListener(events[deviceType].click, onCollapseButton);
+  const onMoveStop = () => {
+    desktop.removeEventListener(events[deviceType].move, onMoveEvent);
+    windowDraggableArea.removeEventListener(events[deviceType].up, onMoveStop);
+    moveElement = false;
+  };
+
+  function onWindowDrag(e) {
+    setCurrentWindowActive();
+    if (e.cancelable) {
+      e.preventDefault();
+    }
+    if (!newWindow.classList.contains('window--fullscreen')) {
+      moveElement = true;
+      initialX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+      initialY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+      desktop.addEventListener(events[deviceType].move, onMoveEvent);
+      windowDraggableArea.addEventListener(events[deviceType].up, onMoveStop);
+    }
   }
-});
+
+  const onCollapseButton = () => {
+    setCurrentWindowActive();
+    if (newWindow.classList.contains('window--collapsed')) {
+      newWindow.classList.remove('window--collapsed');
+      setCurrentWindowActive();
+    } else {
+      if (reference === lastFile) {
+        newWindow.classList.add('window--collapsed');
+      } else {
+        setCurrentWindowActive();
+      }
+    }
+  };
+
+  const onCloseButton = () => {
+    evt.target.classList.add('file');
+    evt.target.classList.remove('file--opened');
+    setCurrentWindowActive();
+    newWindow.remove();
+    pathIcon.remove();
+    reference.remove();
+    clonedTargetContent.remove();
+    referenceIcon.remove();
+    fileLabel.classList.remove('file__label--active');
+    if (offsetVerticalCounter > 0) {
+      offsetVerticalCounter -= 1;
+      initialWindowCounterVertical -= 30;
+    }
+    if (offsetHorisontalCounter > 0) {
+      offsetHorisontalCounter -= 1;
+      initialWindowCounterHorisontal -= 10;
+    }
+  };
+
+  const onExpandButton = () => {
+    setCurrentWindowActive(reference, newWindow);
+    if (newWindow.classList.contains('window--fullscreen')) {
+      newWindow.classList.remove('window--fullscreen');
+    } else {
+      newWindow.classList.add('window--fullscreen');
+    }
+  };
+
+  newWindow.addEventListener(events[deviceType].down, (e) => {
+    if (e.target.closest('.window__draggable-area')) {
+      onWindowDrag(e);
+    }
+    if (e.target.closest('.window__content')) {
+      setCurrentWindowActive();
+    }
+  });
+
+  newWindow.addEventListener(events[deviceType].click, (e) => {
+    if (e.target.closest('.window__button--collapse')) {
+      onCollapseButton();
+    }
+    if (e.target.closest('.window__button--close')) {
+      onCloseButton();
+    }
+    if (e.target.closest('.window__button--expand')) {
+      onExpandButton();
+    }
+  });
+
+  const referenceText = reference.querySelector('.reference__text');
+  referenceText.textContent = fileName.textContent;
+  reference.insertBefore(referenceIcon, referenceText);
+  desktopFooter.appendChild(reference);
+  reference.addEventListener(events[deviceType].click, onCollapseButton);
+}
