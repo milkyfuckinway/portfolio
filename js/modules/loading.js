6182dbf120bfcf0160aad34fcaca7f33842193f1
milkyfuckinway <0-0_0@bk.ru>
2022-12-26 22:49:21 +0300

    Updates

--- js/modules/loading.js
+++ js/modules/loading.js
@@ -2,9 +2,9 @@ const loading = document.querySelector('.loading');
 const removeLoadingScreen = () => {
   loading.remove();
 };
-setTimeout(() => {
-  removeLoadingScreen();
-}, 3000);
+// setTimeout(() => {
+// removeLoadingScreen();
+// }, 3000);
 
 //# sourceMappingURL=loading.js.map
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtb2R1bGVzL2xvYWRpbmcuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgbG9hZGluZyA9IGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJy5sb2FkaW5nJyk7XHJcbmNvbnN0IHJlbW92ZUxvYWRpbmdTY3JlZW4gPSAoKSA9PiB7XHJcbiAgbG9hZGluZy5yZW1vdmUoKTtcclxufTtcclxuc2V0VGltZW91dCgoKSA9PiB7XHJcbiAgcmVtb3ZlTG9hZGluZ1NjcmVlbigpO1xyXG59LCAzMDAwKTtcclxuIl0sImZpbGUiOiJsb2FkaW5nLmpzIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtb2R1bGVzL2xvYWRpbmcuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgbG9hZGluZyA9IGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJy5sb2FkaW5nJyk7XHJcbmNvbnN0IHJlbW92ZUxvYWRpbmdTY3JlZW4gPSAoKSA9PiB7XHJcbiAgbG9hZGluZy5yZW1vdmUoKTtcclxufTtcclxuLy8gc2V0VGltZW91dCgoKSA9PiB7XHJcbi8vIHJlbW92ZUxvYWRpbmdTY3JlZW4oKTtcclxuLy8gfSwgMzAwMCk7XHJcbiJdLCJmaWxlIjoibG9hZGluZy5qcyJ9
