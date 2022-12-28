ec678b42410107fc385ce234cdb452181d79b4d0
milkyfuckinway <0-0_0@bk.ru>
2022-12-28 21:13:32 +0300

    fix

--- src/js/modules/loading.js
+++
@@ -1,9 +0,0 @@
-const loading = document.querySelector('.loading');
-const removeLoadingScreen = () => {
-  loading.remove();
-};
-setTimeout(() => {
-  removeLoadingScreen();
-}, 3000);
-
-removeLoadingScreen();
